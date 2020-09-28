5d96a1207b83f2255728992f8d8377212e062585
Kyle Lynds <kylelynds@Kyles-MBP.lan>
2020-09-28 14:57:48 -0700

    done

--- js/index.js
+++ js/index.js
@@ -40,3 +40,52 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//nav
+const navItems = document.querySelectorAll("nav > a");
+navItems[0].textContent = siteContent["nav"]["nav-item-1"];
+navItems[1].textContent = siteContent["nav"]["nav-item-2"];
+navItems[2].textContent = siteContent["nav"]["nav-item-3"];
+navItems[3].textContent = siteContent["nav"]["nav-item-4"];
+navItems[4].textContent = siteContent["nav"]["nav-item-5"];
+navItems[5].textContent = siteContent["nav"]["nav-item-6"];
+
+//cta
+let ctaHeading = document.querySelector("h1");
+ctaHeading.textContent = siteContent["cta"]["h1"];
+
+let ctaButton = document.querySelector(".cta-text > button");
+ctaButton.textContent = siteContent["cta"]["button"];
+
+let ctaImage = document.querySelector("#cta-img");
+ctaImage.src = siteContent["cta"]["img-src"];
+
+//main-content 
+const topContentH = document.querySelectorAll(".text-content > h4");
+const topContentP = document.querySelectorAll(".text-content > p");
+let mainImg = document.querySelector(".main-content > img");
+
+topContentH[0].textContent = siteContent["main-content"]["features-h4"];
+topContentP[0].textContent = siteContent["main-content"]["features-content"];
+topContentH[1].textContent = siteContent["main-content"]["about-h4"];
+topContentP[1].textContent = siteContent["main-content"]["about-content"];
+mainImg.src = siteContent["main-content"]["middle-img-src"];
+topContentH[2].textContent = siteContent["main-content"]["services-h4"];
+topContentP[2].textContent = siteContent["main-content"]["services-content"];
+topContentH[3].textContent = siteContent["main-content"]["product-h4"];
+topContentP[3].textContent = siteContent["main-content"]["product-content"];
+topContentH[4].textContent = siteContent["main-content"]["vision-h4"];
+topContentP[4].textContent = siteContent["main-content"]["vision-content"];
+
+//contact
+let contactH = document.querySelector(".contact > h4");
+contactH.textContent = siteContent["contact"]["contact-h4"];
+
+const contactP = document.querySelectorAll(".contact > p");
+contactP[0].textContent = siteContent["contact"]["address"];
+contactP[1].textContent = siteContent["contact"]["phone"];
+contactP[2].textContent = siteContent["contact"]["email"];
+
+//footer 
+let foot = document.querySelector("footer > p");
+foot.textContent = siteContent["footer"]["copyright"];
